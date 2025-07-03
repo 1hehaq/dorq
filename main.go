7b8f9d84a7a81d9741b370f6ee34500907a8e6a7
haq <1hehaq@gmail.com>
2025-07-03 16:29:20 +0530

    main

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"os"
+	"strings"
+	"time"
+	"github.com/1hehaq/dorq/search"
+)
+
+var (
+	query   = flag.String("q", "", "dork query or file path")
+	verbose = flag.Bool("v", false, "verbose output")
+	exclude = flag.String("x", "", "exclude engines (comma-separated)")
+	proxy   = flag.String("proxy", "", "proxy URL")
+)
+
+func main() {
+	flag.Parse()
+	engines := search.FilterEngines(*exclude)
+	queries := getQueries()
+	
+	for _, q := range queries {
+		search.Run(q, engines, *proxy, *verbose)
+		time.Sleep(search.RandomDelay(500, 2000))
+	}
+}
+
+func getQueries() []string {
+	if *query == "" {
+		scanner := bufio.NewScanner(os.Stdin)
+		var queries []string
+		for scanner.Scan() {
+			if q := strings.TrimSpace(scanner.Text()); q != "" {
+				queries = append(queries, q)
+			}
+		}
+		return queries
+	}
+	
+	if file, err := os.Open(*query); err == nil {
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		var queries []string
+		for scanner.Scan() {
+			if q := strings.TrimSpace(scanner.Text()); q != "" {
+				queries = append(queries, q)
+			}
+		}
+		return queries
+	}
+	
+	return []string{*query}
+}
